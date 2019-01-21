81eaefc8cd882904a6c492ec1193dcd026144424
epelezaj <epelezaj@ncsu.edu>
2019-01-21 17:01:23 -0500

    Edited formatting

--- ArrayList.cpp
+++ ArrayList.cpp
@@ -5,71 +5,78 @@
 #include <stdio.h>
 
 // ArrayList class
-class ArrayList{
+class ArrayList {
 private:
     Item *array;
     int len = 0;
     int size = 0;
 public:
-    ArrayList(){
+    ArrayList() {
         array = new Item[3];
         size = 3;
         len = 0;
     };
-    ArrayList(const ArrayList &al){
+
+    ArrayList(const ArrayList &al) {
         len = al.len;
         size = al.size;
         auto array = new Item[size];
-        for(int x=0;x<len;x++){
+        for (int x = 0; x < len; x++) {
             array[x] = al.array[x];
         }
     }
-    ~ArrayList(){
+
+    ~ArrayList() {
         delete array;
     }
-    int length(){
+
+    int length() {
         return len;
     }
-    void append(const Item &i){
-        if(len+1>size){
-            auto newarray = new Item[size+3];
-            for(int x=0;x<len;x++){
-                newarray[x]=array[x];
+
+    void append(const Item &i) {
+        if (len + 1 > size) {
+            auto newarray = new Item[size + 3];
+            for (int x = 0; x < len; x++) {
+                newarray[x] = array[x];
             }
             delete array;
             array = newarray;
-            size+=3;
+            size += 3;
         }
         array[len] = i;
         len += 1;
     }
-    bool remove(Item &i){
-        if (len==0) return false;
+
+    bool remove(Item &i) {
+        if (len == 0) return false;
         i = array[0];
-        for(int x=0;x<len-1;x++){
-            array[x]=array[x+1];
+        for (int x = 0; x < len - 1; x++) {
+            array[x] = array[x + 1];
         }
         len--;
         return true;
     }
-    bool empty(){
-        return len==0;
+
+    bool empty() {
+        return len == 0;
     }
-    bool insert(const Item &i, int num){
-        if(num>len+1) return false;
-        if(len+1>size){
-            auto newarray = new Item[size+3];
-            for(int x=0;x<len;x++){
-                newarray[x]=array[x];
+
+    bool insert(const Item &i, int num) {
+        if (num > len + 1) return false;
+        if (len + 1 > size) {
+            auto newarray = new Item[size + 3];
+            for (int x = 0; x < len; x++) {
+                newarray[x] = array[x];
             }
             delete array;
             array = newarray;
-            size+=3;
+            size += 3;
         }
-        for (int x=len+1;x>num;x--){
-            array[x]=array[x-1];
+        for (int x = len + 1; x > num; x--) {
+            array[x] = array[x - 1];
         }
-        array[num-1] = i;
+        array[num - 1] = i;
         len++;
         return true;
     }
