4048953449b09d62afa57176fc9b94059608f2ed
epelezaj <epelezaj@ncsu.edu>
2019-01-21 13:33:38 -0500

    Finished LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,13 +1,11 @@
 // ECE 209, Spring 2019
 // Homework 2, Problem 2
 
-// LinkedList class
-
-// main function to demonstrate class
 #include "Item.h"
+#include <stdio.h>
 
-
-class list {
+//Linked List class
+class List {
 private:
     class ListNode {
     private:
@@ -35,12 +33,12 @@ private:
     ListNode *head;
     ListNode *tail;
 public:
-    list() {
+    List() {
         head = nullptr;
         tail = nullptr;
     }
 
-    list(const list &l) {
+    List(const List &l) {
         head = new ListNode(l.head->getItem());
         ListNode *newlist = head;
         ListNode *oldlist = l.head;
@@ -52,7 +50,7 @@ public:
         tail = new ListNode(l.tail->getItem());
     }
 
-    ~list() {
+    ~List() {
         Item t;
         while (!empty()) {
             remove(t);
@@ -62,7 +60,7 @@ public:
     int length() {
         int count = 0;
         ListNode *i = head;
-        while (!i) {
+        while (i) {
             i = i->getNext();
             count++;
         }
@@ -99,17 +97,46 @@ public:
     }
 
     bool insert(const Item &i, int num) {
+        if (num > length() + 1) return false;
+        ListNode *next = head;
+        ListNode *prev = nullptr;
         int x;
-        ListNode *p = head;
-        auto node = new ListNode(i);
-        for (x = num; x > 0; x--) {
-            if (!(p = p->getNext())) return false;
+        for (x = 0; x < num - 1; x++) {
+            prev = next;
+            next = next->getNext();
         }
-        p->setNext(node);
+        auto newNode = new ListNode(i);
+        newNode->setNext(next);
+        prev->setNext(newNode);
         return true;
     }
 };
 
+// main function to demonstrate class
 int main() {
+    List list1; //uses default constructor to create an empty list
+    const char *first = "first string";
+    Item itema(1, first);
+    list1.append(itema); //appends to list
+
+    const char *second = "second string";
+    Item itemb(2, second);
+    list1.append(itemb); //appends to list
+
+    const char *third = "third string";
+    Item itemc(3, third);
+    list1.append(itemc); //append to list
+
+    printf("The list is currently %d nodes long\n", list1.length()); //prints length of list
+    Item copy;
+    list1.remove(copy); //removes first node of the list
+    printf("Item #%d was removed\n", copy.getKey());
+    printf("The list is currently %d nodes long\n", list1.length());
+    list1.insert(itema, 3); //inserts item into third node of list
+    list1.empty();
+    printf("The list is currently %d nodes long\n", list1.length());
+
+    List list2 = list1;
+    printf("The copied list is currently %d nodes long\n", list2.length());
     return 0;
 }
